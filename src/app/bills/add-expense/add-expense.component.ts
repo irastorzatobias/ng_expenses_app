981a705104cb5ddc7c4fa9e80456507b961783e0
tobias <tobirastorza@gmail.com>
2023-08-21 03:36:08 -0300

    mock modal;

--- src/app/bills/add-expense/add-expense.component.ts
+++ src/app/bills/add-expense/add-expense.component.ts
@@ -4,4 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-add-expense',
   templateUrl: './add-expense.component.html',
 })
-export class AddExpenseComponent {}
+export class AddExpenseComponent {
+  showModal: Boolean = false;
+}
